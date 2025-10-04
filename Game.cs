c00852c70f78c24951591519065b5b05fa86c03e
mkj777 <maxikie02@gmail.com>
2025-10-04 18:49:00 +0200

    feat: added datetime to class game and changed comments

--- Game.cs
+++ Game.cs
@@ -1,17 +1,22 @@
-﻿namespace Codec
+﻿using System;
+
+namespace Codec
 {
     public class Game
     {
-        // The display name of the game
+        // the display name of the game
         public string Name { get; set; }
 
-        // The full path to the executable
+        // the full path to the executable
         public string Executable { get; set; }
 
-        // Path to the Cover, can be a URL or local path
+        // path to the Cover, can be a URL or local path
         public string Cover { get; set; }
 
-        // Optional: Path to .bat- or .ps1 Script to launch the game
+        // optional: Path to .bat- or .ps1 Script to launch the game
         public string? LaunchScript { get; set; }
+
+        // to save when the game was last played
+        public DateTime? LastPlayed { get; set; }
     }
 }
