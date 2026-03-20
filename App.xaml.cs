e570fd78532b00372dc50bfc34baf2dd5ca93c23
Maximilian Kielholz <maxikie02@gmail.com>
2026-03-20 06:43:49 +0100

    removed template comment

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using Codec.Views;
 using Microsoft.UI.Xaml;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Codec
 {
     /// <summary>
